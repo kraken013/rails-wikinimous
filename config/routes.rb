c9199bc1ad814d22c862f246eebad4e1a843a447
sofiane flici <sofiane.flici14@gmail.com>
2023-06-13 16:18:03 +0200

    wikinimous

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "articles#index"
+
+  get "articles/new", to: "articles#new", as: :new_article
+  post "articles", to: "articles#create"
+  get "articles/:id", to: "articles#show", as: :article
+  get "articles/:id/edit", to: "articles#edit", as: :edit_article
+  patch "articles/:id", to: "articles#update"
+  delete "articles/:id", to: "articles#destroy"
 end
